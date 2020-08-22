1617f67817d4eda9d149489da17bed1f5330427c
Eric Silva <eric.silva@external.fcagroup.com>
2020-08-22 12:24:21 -0300

    Estrutura o projeto spring

---
+++ src/main/java/br/com/intergado/entrevista/ericsilva/EricsilvaApplication.java
@@ -0,0 +1,13 @@
+package br.com.intergado.entrevista.ericsilva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EricsilvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EricsilvaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/intergado/entrevista/ericsilva/EricsilvaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.intergado.entrevista.ericsilva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EricsilvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
